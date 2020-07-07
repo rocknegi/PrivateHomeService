9568cba3c4f563c2f661b823ae80759d7b48a11b
unknown <rocknegi53@gmail.com>
2020-07-07 16:17:12 +0530

    add DOB

--- src/components/login/Profile.js
+++ src/components/login/Profile.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, SafeAreaView, StyleSheet, ScrollView, TouchableOpacity, ToastAndroid, TextInput } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/Feather'
+import firebase from 'react-native-firebase';
+import DatePicker from 'react-native-datepicker'
+import moment from 'moment';
 
 import { PrimayColor } from '../theme/Colors'
 
@@ -9,25 +12,44 @@ export default function Profile({ navigation }) {
 
     const [data, setData] = useState({
         phoneNo: '',
-        username: ''
+        username: '',
+        dob: ''
     });
+    const [date, setDate] = useState(moment().format('DD-MM-YYYY'),)
 
     const getStoredData = async () => {
         const phoneNo = await AsyncStorage.getItem('phoneNo');
-        const username = await AsyncStorage.getItem('username');
-        setData({ phoneNo, username })
+        // const username = await AsyncStorage.getItem('username');
+        setData({ ...data, phoneNo });
+        // if (username) {
+        //     setData({ ...data, username })
+        // }
     }
 
     const update = async () => {
 
         AsyncStorage.setItem('phoneNo', data.phoneNo);
         AsyncStorage.setItem('username', data.username);
-        ToastAndroid.showWithGravity("Updated", ToastAndroid.SHORT, ToastAndroid.BOTTOM)
+        ToastAndroid.showWithGravity("Updated", ToastAndroid.SHORT, ToastAndroid.BOTTOM);
+
+        firebase.firestore().collection('Users').doc(data.phoneNo).update({
+            dob: date,
+            username: data.username
+        })
     }
 
     useEffect(() => {
-        getStoredData()
+        getStoredData();
     }, []);
+
+    useEffect(() => {
+        firebase.firestore().collection('Users').doc(data.phoneNo).get().then(doc => {
+            if (doc.exists) {
+                setDate(doc.data()['dob']);
+                setData({ ...data, username: doc.data()['username'] })
+            }
+        });
+    }, [data.phoneNo])
 
     return (
         <SafeAreaView style={styles.container}>
@@ -60,6 +82,35 @@ export default function Profile({ navigation }) {
                                 onChangeText={value => setData({ ...data, username: value })}
                             />
                         </View>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+                        <View style={[styles.textContainer, { backgroundColor: '#fafafa', flex: 0.73, marginHorizontal: 0 }]}>
+                            <Text style={{ fontSize: 15, fontWeight: '100' }}>Date of birth</Text>
+                        </View>
+                        <DatePicker
+                            style={{ marginTop: 10, left: 15 }}
+                            date={date}
+                            mode="date"
+                            placeholder="select date"
+                            format="DD-MM-YYYY"
+                            confirmBtnText="Confirm"
+                            cancelBtnText="Cancel"
+                            customStyles={{
+                                dateIcon: {
+                                    position: 'absolute',
+                                    left: 0,
+                                    top: 4,
+                                    marginLeft: 0
+                                },
+                                dateInput: {
+                                    paddingLeft: 10,
+                                    borderColor: PrimayColor,
+                                    borderWidth: 2
+                                }
+                            }}
+                            onDateChange={(date) => setDate(date)}
+                        />
                     </View>
                     <TouchableOpacity style={styles.button} onPress={update}>
                         <Text style={styles.buttonText}>Update</Text>
